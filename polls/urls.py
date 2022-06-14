78156e65613c566aadb5ef58fd1df460d903fb27
Mohammed R <mohammed.rmich@etu.univ-tours.fr>
2022-06-14 10:25:53 +0200

    ignore some files

--- polls/urls.py
+++ polls/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     # the url will be : localhost:8000/polls/additional_parameter
     # the second parameter calls the specified view function with an
     # HttpRequest object as the first argument
-    # the third parameter allows the URL to be referred to from elsewhere
+    # the third parameter allows the URL to lbe referred to from elsewhere
     path('', views.index, name='index'),
 
 ]
