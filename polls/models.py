c3c73edab46d2399350a4d816541018474f10887
Mohammed R <mohammed.rmich@etu.univ-tours.fr>
2022-06-14 12:14:54 +0200

    override str + another method

--- polls/models.py
+++ polls/models.py
@@ -1,12 +1,25 @@
+import datetime
+
 from django.db import models
+from django.db.models import DateTimeField
+from django.utils import timezone
 
 
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     publication_date = models.DateTimeField('publish date')
 
+    def __str__(self):
+        return f'Question : {self.question_text}, Publication date : {self.publication_date}'
+
+    def was_published_recently(self) -> bool:
+        return self.publication_date.timestamp() >= (timezone.now() - datetime.timedelta(days=1)).timestamp()
+
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return f'Choice : {self.choice_text}, Votes : {self.votes}'
